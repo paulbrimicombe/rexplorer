44c261117c757eb26fd61aed11d26147d56f5099
Paul Douglas Brimicombe <paul.brimicombe@bbc.co.uk>
2020-11-28 20:58:25 +0000

    Updates

---
+++ client/index.9bd3d7a7.js
@@ -0,0 +1,1 @@
+import{r as t}from"./client.57a26625.js";function a(a,{delay:e=0,duration:o=400,easing:i=t}){const n=+getComputedStyle(a).opacity;return{delay:e,duration:o,easing:i,css:t=>"opacity: "+t*n}}export{a as f};
