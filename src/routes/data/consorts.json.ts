cc5f88ca8bc5d261d4ceb90c858038510f7a0d89
Paul Douglas Brimicombe <paul.brimicombe@bbc.co.uk>
2024-03-25 08:23:11 +0000

    Attempt to fix export missing data files

--- src/routes/data/consorts.json.ts
+++ src/routes/data/consorts.json.ts
@@ -1,11 +1,10 @@
 import type { ServerResponse } from "http";
-import RatingsLoader from "./_RatingsLoader";
+import { scores } from "./_consorts.json";
 
 export async function get(_, res: ServerResponse) {
-  const data = await RatingsLoader.load({ type: "consorts" });
   res.writeHead(200, {
     "Content-Type": "application/json",
   });
 
-  res.end(JSON.stringify(data));
+  res.end(JSON.stringify({ scores }));
 }
--- src/routes/data/monarchs.json.ts
+++ src/routes/data/monarchs.json.ts
@@ -1,11 +1,10 @@
 import type { ServerResponse } from "http";
-import RatingsLoader from "./_RatingsLoader";
+import { scores } from "./_monarchs.json";
 
 export async function get(_, res: ServerResponse) {
-  const data = await RatingsLoader.load({ type: "monarchs" });
   res.writeHead(200, {
     "Content-Type": "application/json",
   });
 
-  res.end(JSON.stringify(data));
+  res.end(JSON.stringify({ scores }));
 }
